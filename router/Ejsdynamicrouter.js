a584041148adb398f14e05295d7fd207cd7b255e
Iamvikassingh <9625vikassingh@gmail.com>
2025-06-02 13:39:54 +0530

    new upload folder create and path is added

--- router/Ejsdynamicrouter.js
+++ router/Ejsdynamicrouter.js
@@ -7,7 +7,7 @@ const crypto = require('crypto');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/images/uploads')
+        cb(null, './uploads')
     },
     filename: function (req, file, cb) {
         crypto.randomBytes(12, function (err, bytes){
